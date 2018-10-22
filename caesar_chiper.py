0b0779509c5daf05a28a2789e0e6ee8bcf734894
Ireaurora <ireaurora@gmail.com>
2018-10-22 19:01:30 +0100

    I've updated the caesar chiper and it now encrypts long sentence without a problem. There are still a few problems with the decryption given the nature of how I defined the alphabet

--- caesar_chiper.py
+++ caesar_chiper.py
@@ -1,14 +1,19 @@
 import string
-alphabet = list(string.ascii_lowercase)
-
-initial_message = str(input("What would you like to shift?"))
+alphabet = list(string.ascii_lowercase) + [' '] + [' ']
+print(alphabet)
+initial_message = str(input("What would you like to encrypt?"))
+second_message = str(input("What would you like to decrypt?"))
 
 message = list(initial_message)
-
+message2 = list(second_message)
 array = []
-
 letterarray = []
 
+def deciding():
+    if initial_message == '':
+        return final(y)
+    else:
+        return final1(z)
 
 def shifting():
     for letter in message:
@@ -20,7 +25,22 @@ y = shifting()
 
 def final(y):
     for number in y:
+         letterarray.append(str(alphabet[number]))
+    return ''.join(letterarray)
+
+
+
+def shifting1():
+    for letter in message2:
+        indexing = alphabet.index(letter) - 1
+        array.append(int(indexing))
+    return array
+
+z = shifting1()
+
+def final1(z):
+    for number in z:
         letterarray.append(str(alphabet[number]))
     return ''.join(letterarray)
 
-print(final(y))
+print(deciding())
