b1658a2230020990018fde7bc50892b54d126eb9
Ireaurora <ireaurora@gmail.com>
2018-10-22 22:15:57 +0100

    Adding the right file

--- checking_for_palindromes.py
+++ checking_for_palindromes.py
@@ -5,26 +5,6 @@ alphabet = list(string.ascii_lowercase)
 initial_message = str(input("What word would you like to check?"))
 initial_message = initial_message.lower()
 
-def more_complex_wordspace(initial_message):
-    if initial_message.index(" ") != -1:
-        initial_message = [s.replace(" ", "") for s in initial_message]
-        return final(str(initial_message))
-    else:
-        return "Not sure how to proceed"
-
-def more_complex_wordexclamative(initial_message):
-    if initial_message.index(" ") != -1:
-        initial_message = [s.replace(" ", "") for s in initial_message]
-        return final(str(initial_message))
-    else:
-        return "Not sure how to proceed"
-
-def more_complex_wordcomma(initial_message):
-    if initial_message.index(" ") != -1:
-        initial_message = [s.replace(" ", "") for s in initial_message]
-        return final(str(initial_message))
-    else:
-        return "Not sure how to proceed"
 
 def reversing_word(initial_message):
     reversed_string = initial_message[::-1]
@@ -34,15 +14,39 @@ def reversing_word(initial_message):
         return list(initial_message) + [": is not a palidrome, because "] + list(reversed_string)
 
 
+def more_complex_wordspace(initial_message):
+    if initial_message.index(" ") != -1:
+        initial_message = [s.replace(" ", "") for s in initial_message]
+        return final(initial_message)
+    else:
+        return "Not sure how to proceed"
+
+def more_complex_wordexclamative(initial_message):
+    if initial_message.index(" ") != -1:
+        initial_message = [s.replace(" ", "") for s in initial_message]
+        return final(initial_message)
+    else:
+        return "Not sure how to proceed"
+
+def more_complex_wordcomma(initial_message):
+    if initial_message.index(" ") != -1:
+        initial_message = [s.replace(" ", "") for s in initial_message]
+        return final(initial_message)
+    else:
+        return "Not sure how to proceed"
+
+
 def final(initial_message):
     if initial_message.find(" ") == -1 and initial_message.find("!") == -1 and initial_message.find(",") == -1:
-        return reversing_word(initial_message)
-    if initial_message.find(" ") != -1:
-        if initial_message.find("!") != -1:
-            if initial_message.find(",") != -1:
-                return more_complex_wordcomma(initial_message)
-            return more_complex_wordexclamative(initial_message)
+       return reversing_word(initial_message)
+    while initial_message.index("") != 1:
         return more_complex_wordspace(initial_message)
+    while initial_message.index("!") != 1:
+        return more_complex_wordexclamative(initial_message)
+    while initial_message.index(",") != 1:
+        return more_complex_wordcomma(initial_message)
+
+
 
 
 print(final(initial_message))
