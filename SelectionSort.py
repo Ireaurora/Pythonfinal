5754457aca67c3b4c20732222563c94ca3c05c11
Ireaurora <ireaurora@gmail.com>
2019-02-11 23:02:45 +0000

    Now the function works

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,4 +1,15 @@
 #declare the string you want to order
 listing = list(input("What list would you like to order?"))
 
-def SelectionSort():
+def SelectionSort(listing):
+    for pointer_a in range(len(listing)):
+        min = pointer_a;
+        for pointer_b in range(pointer_a + 1, len(listing)):
+                if listing[pointer_b] < listing[min]:
+                    min = pointer_b
+        c = listing[pointer_a]
+        listing[pointer_a] = listing[min]
+        listing[min] = c
+    return listing
+
+print(SelectionSort(listing))
