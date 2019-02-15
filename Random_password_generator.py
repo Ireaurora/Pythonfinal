75dfd6996e296daa61eb59db2cc115069eaf83ab
Ireaurora <ireaurora@gmail.com>
2019-02-15 22:34:12 +0000

    Now the password has to be of a certain lenght

--- Random_password_generator.py
+++ Random_password_generator.py
@@ -3,11 +3,19 @@ import random
 
 password_size = int(input("How long would you like your password to be?"))
 
+def checking_lenght():
+    if password_size < 6:
+        print(checked())
+    else:
+        print(random_password())
+
 def random_password():
     characters = string.ascii_uppercase + string.ascii_lowercase + string.digits
     return ''.join(random.choice(characters)for x in range (0, password_size))
 
 
+def checked():
+    password_size = int(input("How long would you like your password to be?"))
+    return checking_lenght()
 
-
-print(random_password())
+print(checking_lenght())
