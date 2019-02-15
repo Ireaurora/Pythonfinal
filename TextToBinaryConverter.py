5fa4f716e98da398a1baca376af7931cf94efe25
Ireaurora <ireaurora@gmail.com>
2019-02-15 18:57:13 +0000

    This text to binary converter is working

--- TextToBinaryConverter.py
+++ TextToBinaryConverter.py
@@ -1,1 +1,6 @@
 #this program is a text to binary converter
+print(">>>>>>>>>>>>>Text to Binary Convertor<<<<<<<<<<<<")
+
+text = input("Enter text to convert to binary")
+
+print(''.join(format(ord(x), 'b') for x in text))
